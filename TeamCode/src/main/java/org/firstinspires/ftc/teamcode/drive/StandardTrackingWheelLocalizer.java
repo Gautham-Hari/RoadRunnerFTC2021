dbbe907ede0a46e02535b38c2a8b8612c89743c2
GauthamHari000 <[gautham.hari4@gmail.com]>
2021-04-24 23:56:34 -0400

    A

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/StandardTrackingWheelLocalizer.java
@@ -28,11 +28,11 @@ import java.util.List;
 @Config
 public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer {
     public static double TICKS_PER_REV = 8192;
-    public static double WHEEL_RADIUS = 1.5; // in
+    public static double WHEEL_RADIUS = 1.771655; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 16.25; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = 2.375; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 16.125; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = 2.5; // in; offset of the lateral wheel
 
     private Encoder odoRY, odoLY, odoX;
     public static double X_MULTIPLIER = 1; // Multiplier in the X direction
@@ -45,11 +45,12 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
                 new Pose2d(FORWARD_OFFSET, 0, Math.toRadians(90)) // front
         ));
 
-        odoRY = new Encoder(hardwareMap.get(DcMotorEx.class, "odoRY"));
-        odoLY = new Encoder(hardwareMap.get(DcMotorEx.class, "odoLY"));
-        odoX = new Encoder(hardwareMap.get(DcMotorEx.class, "odoX"));
+        odoRY = new Encoder(hardwareMap.get(DcMotorEx.class, "rightf"));
+        odoLY = new Encoder(hardwareMap.get(DcMotorEx.class, "ringPickup2"));
+        odoX = new Encoder(hardwareMap.get(DcMotorEx.class, "ringPickup"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
+
 
         //////////////////////////////////////////
 
